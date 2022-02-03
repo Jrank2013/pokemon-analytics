c56e1ad9ae3472f491f6ca6f87f607934d156de3
Jamario Rankins <jamariorankins@gmail.com>
2022-02-02 21:08:36 -0700

    Set axios rate limit to 10 per second

--- lib/http.ts
+++ lib/http.ts
@@ -2,8 +2,7 @@ import axiosRateLimit from "axios-rate-limit";
 import axios from "axios";
 
 
-const http = axiosRateLimit(axios.create(), {maxRequests: 4, perMilliseconds: 1000})
+const http = axiosRateLimit(axios.create(), {maxRequests: 10, perMilliseconds: 1000})
 
-export {}
 
 export default http
